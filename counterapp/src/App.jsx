7e1e92d1b76fe22319267e39ddff87a844c131c2
bindhushree <bindhushreeb05@gmail.com>
2024-02-02 09:21:52 +0530

    done

--- counterapp/src/App.jsx
+++ counterapp/src/App.jsx
@@ -1,29 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+import { Component } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class CounterApp extends Component{
+  constructor(){
+    super();
 
-  return (
-    <>
+    this.state = {
+      counter : 0,
+    }
+  }
+
+  handleDecrease = ()=>{
+    this.setState({
+      counter:this.state.counter-1
+    })
+  }
+
+  handleIncrease = ()=>{
+    this.setState({
+      counter:this.state.counter+1
+    })
+  }
+
+  handleReset = ()=>{
+    this.setState({
+      counter:0
+    })
+  }
+
+
+  render(){
+    return(
+      <div className="counter">
       <h1>Counter App</h1>
-      <div>
-        <h1>{count}</h1>
+      <span className="counter__output">{this.state.counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={this.handleIncrease}>+</button>
+        <button className="control__btn" onClick={this.handleDecrease}>-</button>
+        <button className="reset" onClick={this.handleReset}>Reset</button>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          +
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          -
-        </button>
-        <button onClick={() => setCount((count) => 0)}>
-          Reset
-        </button>
-      </div>
-
-    </>
-  )
+    </div>
+    )
+  }
 }
-
-export default App
